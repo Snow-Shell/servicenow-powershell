0b2c7fcee6dfec33595fd99dcd9dda83e973d617
Rick-2CA <Rick-2CA@users.noreply.github.com>
2017-03-06 12:45:30 -0600

    MAKE Update:  V1.12

--- MAKE.ps1
+++ MAKE.ps1
@@ -66,7 +66,7 @@ $PackageFilePatternExclusions = @(
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-$Version = "0.1.5"
+$Version = "0.1.12"
 $ModuleName = "PSServiceNow"
 $PackageName = "$ModuleName-v$($version).zip";
 
