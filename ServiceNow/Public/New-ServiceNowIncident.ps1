bc2eb2cc346065affa61a8f4ca81bb3ac1347552
Rick-2CA <Rick-2CA@users.noreply.github.com>
2017-08-30 18:05:46 -0500

    Cleaned up New-ServiceNowIncident.  Added error checking for duplicate incident fields

--- ServiceNow/Public/New-ServiceNowIncident.ps1
+++ ServiceNow/Public/New-ServiceNowIncident.ps1
@@ -2,16 +2,17 @@ function New-ServiceNowIncident{
     Param(
 
         # sys_id of the caller of the incident (user Get-ServiceNowUser to retrieve this)
-        [parameter(ParameterSetName='SpecifyConnectionFields', mandatory=$true)]
-        [parameter(ParameterSetName='UseConnectionObject', mandatory=$true)]
-        [parameter(ParameterSetName='SetGlobalAuth', mandatory=$true)]
+        [parameter(Mandatory=$true)]
+        [parameter(ParameterSetName='SpecifyConnectionFields')]
+        [parameter(ParameterSetName='UseConnectionObject')]
+        [parameter(ParameterSetName='SetGlobalAuth')]
         [string]$Caller,
         
         # Short description of the incident
-        [parameter(mandatory=$true)]
-        [parameter(ParameterSetName='SpecifyConnectionFields', mandatory=$true)]
-        [parameter(ParameterSetName='UseConnectionObject', mandatory=$true)]
-        [parameter(ParameterSetName='SetGlobalAuth', mandatory=$true)]
+        [parameter(Mandatory=$true)]
+        [parameter(ParameterSetName='SpecifyConnectionFields')]
+        [parameter(ParameterSetName='UseConnectionObject')]
+        [parameter(ParameterSetName='SetGlobalAuth')]
         [string]$ShortDescription,
 
         # Long description of the incident
@@ -66,50 +67,76 @@ function New-ServiceNowIncident{
         # Credential used to authenticate to ServiceNow  
         [Parameter(ParameterSetName='SpecifyConnectionFields', Mandatory=$True)]
         [ValidateNotNullOrEmpty()]
-        [PSCredential]
-        $ServiceNowCredential, 
+        [PSCredential]$ServiceNowCredential, 
 
         # The URL for the ServiceNow instance being used (eg: instancename.service-now.com)
         [Parameter(ParameterSetName='SpecifyConnectionFields', Mandatory=$True)]
         [ValidateNotNullOrEmpty()]
-        [string]
-        $ServiceNowURL, 
+        [string]$ServiceNowURL, 
 
         #Azure Automation Connection object containing username, password, and URL for the ServiceNow instance
         [Parameter(ParameterSetName='UseConnectionObject', Mandatory=$True)] 
         [ValidateNotNullOrEmpty()]
-        [Hashtable]
-        $Connection
+        [Hashtable]$Connection
     )
 
-   
-    $Values = @{
-        'caller_id' = $Caller
-        'short_description' = $ShortDescription
-        'description' = $Description
-        'assignment_group' = $AssignmentGroup
-        'comments' = $Comment
-        'category' = $Category
-        'subcategory' = $Subcategory
-        'cmdb_ci' = $ConfigurationItem
+    # Create a hash table of any defined parameters (not CustomFields) that have values
+    $DefinedIncidentParameters = @('AssignmentGroup','Caller','Category','Comment','ConfigurationItem','Description','ShortDescription','Subcategory')
+    $TableEntryValues = @{}
+    ForEach ($Parameter in $DefinedIncidentParameters) {
+        If ($null -ne $PSBoundParameters.$Parameter) {
+            # Turn the defined parameter name into the ServiceNow attribute name
+            $KeyToAdd = Switch ($Parameter) {
+                AssignmentGroup     {'assignment_group'}
+                Caller              {'caller_id'}
+                Category            {'category'}
+                Comment             {'comments'}
+                ConfigurationItem   {'cmdb_ci'}
+                Description         {'description'}
+                ShortDescription    {'short_description'}
+                Subcategory         {'subcategory'}
+            }
+            $TableEntryValues.Add($KeyToAdd,$PSBoundParameters.$Parameter)
+        }
     }
     
-    if($CustomFields)
-    {
-        $Values += $CustomFields 
+    # Add CustomFields hash pairs to the Table Entry Values hash table
+    If ($null -ne $PSBoundParameters.CustomFields) {
+        $DuplicateTableEntryValues = ForEach ($Key in $CustomFields.Keys) {
+            If (($TableEntryValues.ContainsKey($Key) -eq $False)) {
+                # Add the unique entry to the table entry values hash table
+                $TableEntryValues.Add($Key,$CustomFields[$Key])
+            }
+            Else {
+                # Capture the duplicate key name
+                $Key
+            }
+        }
     }
-    
-    if ($Connection -ne $null)
-    {
-       New-ServiceNowTableEntry -Table 'incident' -Values $Values -Connection $Connection
-    }
-    elseif ($ServiceNowCredential -ne $null -and $ServiceNowURL -ne $null) 
-    {
-       New-ServiceNowTableEntry -Table 'incident' -Values $Values -ServiceNowCredential $ServiceNowCredential -ServiceNowURL $ServiceNowURL
-    }
-    else 
-    {
-       New-ServiceNowTableEntry -Table 'incident' -Values $Values   
-    } 
 
+    # Throw an error if duplicate fields were provided
+    If ($null -ne $DuplicateTableEntryValues) {
+        $DuplicateKeyList = $DuplicateTableEntryValues -join ","
+        Throw "Ticket fields may only be used once:  $DuplicateKeyList"
+    }
+    
+    # Table Entry Splat
+    $newServiceNowTableEntrySplat = @{
+        Table = 'incident'
+        Values = $TableEntryValues
+    }
+
+    # Update the splat if the parameters have values
+    if ($null -ne $PSBoundParameters.Connection)
+    {     
+        $newServiceNowTableEntrySplat.Add('Connection',$Connection)
+    }
+    elseif ($null -ne $PSBoundParameters.ServiceNowCredential -and $null -ne $PSBoundParameters.ServiceNowURL) 
+    {
+        $newServiceNowTableEntrySplat.Add('ServiceNowCredential',$ServiceNowCredential)
+        $newServiceNowTableEntrySplat.Add('ServiceNowURL',$ServiceNowURL)
+    }
+       
+    # Create the table entry
+    New-ServiceNowTableEntry @newServiceNowTableEntrySplat
 }
