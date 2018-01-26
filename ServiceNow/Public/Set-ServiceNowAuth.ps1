227095de958bf458206328969fa2552e12ffe0fa
GilmondRoss <ross.meyer@gilmond.com>
2018-01-26 01:14:39 +0000

    Update Set-ServiceNowAuth.ps1
    Fixes #2.

--- ServiceNow/Public/Set-ServiceNowAuth.ps1
+++ ServiceNow/Public/Set-ServiceNowAuth.ps1
@@ -1,13 +1,22 @@
-function Set-ServiceNowAuth{
+function Set-ServiceNowAuth {
+    [CmdletBinding()]
     param(
-        [parameter(mandatory=$true)]
+        [parameter(mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( { if ($_ -notlike 'https://*') {
+                    $true
+                }
+                else {
+                    Throw "Please exclude https:// from your URL parameter: $_ "
+                } })]
         [string]$url,
         
-        [parameter(mandatory=$true)]
+        [parameter(mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSCredential]$Credentials
     )
     $Global:ServiceNowURL = 'https://' + $url
     $Global:ServiceNowRESTURL = $ServiceNowURL + '/api/now/v1'
     $Global:ServiceNowCredentials = $credentials
-    return $true;
+    return $true
 }
