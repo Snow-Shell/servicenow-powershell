6bbb85650f0024db05531411ccb525f74c878446
Rick-2CA <Rick-2CA@users.noreply.github.com>
2018-12-27 16:33:44 -0600

    Apply 'lean' parameter standardization to all Get functions

--- ServiceNow/Public/Get-ServiceNowTableEntry.ps1
+++ ServiceNow/Public/Get-ServiceNowTableEntry.ps1
@@ -22,45 +22,46 @@ function Get-ServiceNowTableEntry {
     [CmdletBinding(DefaultParameterSetName)]
     param(
         # Table containing the entry we're deleting
-        [parameter(mandatory=$true)]
+        [parameter(mandatory = $true)]
         [string]$Table,
 
         # Machine name of the field to order by
-        [parameter(mandatory = $false)]
+        [parameter(Mandatory = $false)]
         [string]$OrderBy = 'opened_at',
 
         # Direction of ordering (Desc/Asc)
-        [parameter(mandatory = $false)]
-        [ValidateSet("Desc", "Asc")]
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Desc', 'Asc')]
         [string]$OrderDirection = 'Desc',
 
         # Maximum number of records to return
-        [parameter(mandatory = $false)]
+        [parameter(Mandatory = $false)]
         [int]$Limit = 10,
 
         # Hashtable containing machine field names and values returned must match exactly (will be combined with AND)
-        [parameter(mandatory = $false)]
+        [parameter(Mandatory = $false)]
         [hashtable]$MatchExact = @{},
 
         # Hashtable containing machine field names and values returned rows must contain (will be combined with AND)
-        [parameter(mandatory = $false)]
+        [parameter(Mandatory = $false)]
         [hashtable]$MatchContains = @{},
 
         # Whether or not to show human readable display values instead of machine values
-        [parameter(mandatory = $false)]
-        [ValidateSet("true", "false", "all")]
+        [parameter(Mandatory = $false)]
+        [ValidateSet('true', 'false', 'all')]
         [string]$DisplayValues = 'true',
 
-        [Parameter(ParameterSetName = 'SpecifyConnectionFields', Mandatory = $True)]
+        [Parameter(ParameterSetName = 'SpecifyConnectionFields', Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [Alias('ServiceNowCredential')]
         [PSCredential]$Credential,
 
-        [Parameter(ParameterSetName = 'SpecifyConnectionFields', Mandatory = $True)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter(ParameterSetName = 'SpecifyConnectionFields', Mandatory = $true)]
+        [ValidateScript({Test-ServiceNowURL -Url $_})]
+        [Alias('Url')]
         [string]$ServiceNowURL,
 
-        [Parameter(ParameterSetName = 'UseConnectionObject', Mandatory = $True)]
+        [Parameter(ParameterSetName = 'UseConnectionObject', Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [hashtable]$Connection
     )
