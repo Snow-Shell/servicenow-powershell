ed7c3722f523093b35d6cce493c4f184d448054f
Kaloszer <seb.wiszowaty@gmail.com>
2020-04-25 17:03:30 +0200

    Added MatchIn to get-servicenowchangerequest and configurationitem

--- ServiceNow/Public/Get-ServiceNowConfigurationItem.ps1
+++ ServiceNow/Public/Get-ServiceNowConfigurationItem.ps1
@@ -28,6 +28,10 @@ function Get-ServiceNowConfigurationItem {
         [Parameter(Mandatory = $false)]
         [hashtable]$MatchContains = @{},
 
+        # Hashtable containing machine field names and values returned rows must contain (will be combined with AND)
+        [Parameter(Mandatory = $false)]
+        [hashtable]$MatchIn = @{},
+
         # Whether or not to show human readable display values instead of machine values
         [Parameter(Mandatory = $false)]
         [ValidateSet('true', 'false', 'all')]
@@ -54,6 +58,7 @@ function Get-ServiceNowConfigurationItem {
         MatchExact      = $MatchExact
         OrderDirection  = $OrderDirection
         MatchContains   = $MatchContains
+        MatchIn         = $MatchIn
     }
     $Query = New-ServiceNowQuery @newServiceNowQuerySplat
 
