a51aa8b84abc0222d87d2010f7aa755a2200c28b
Greg Brownstein <greg@jagtechnical.com>
2022-06-14 22:36:03 -0400

    docker init

--- ServiceNow/Private/Get-ServiceNowAuth.ps1
+++ ServiceNow/Private/Get-ServiceNowAuth.ps1
@@ -35,8 +35,7 @@ function Get-ServiceNowAuth {
                 $hashOut.Headers = @{
                     'Authorization' = 'Bearer {0}' -f $ServiceNowSession.AccessToken.GetNetworkCredential().password
                 }
-            }
-            else {
+            } else {
                 $hashOut.Credential = $ServiceNowSession.Credential
             }
 
@@ -44,18 +43,27 @@ function Get-ServiceNowAuth {
                 $hashOut.Proxy = $ServiceNowSession.Proxy
                 if ( $ServiceNowSession.ProxyCredential ) {
                     $hashOut.ProxyCredential = $ServiceNowSession.ProxyCredential
-                }
-                else {
+                } else {
                     $hashOut.ProxyUseDefaultCredentials = $true
                 }
             }
-        }
-        elseif ( $Connection ) {
+        } elseif ( $Connection ) {
             Write-Verbose 'connection'
             $SecurePassword = ConvertTo-SecureString $Connection.Password -AsPlainText -Force
             $Credential = New-Object System.Management.Automation.PSCredential ($Connection.Username, $SecurePassword)
             $hashOut.Credential = $Credential
             $hashOut.Uri = 'https://{0}/api/now/v1' -f $Connection.ServiceNowUri
+        } elseif ( $env:SNOW_SERVER ) {
+            $hashOut.Uri = 'https://{0}/api/now' -f $env:SNOW_SERVER
+            if ( $env:SNOW_TOKEN ) {
+                $hashOut.Headers = @{
+                    'Authorization' = 'Bearer {0}' -f $env:SNOW_TOKEN
+                }
+            } elseif ( $env:SNOW_USER -and $env:SNOW_PASS ) {
+                $hashOut.Credential = New-Object System.Management.Automation.PSCredential($env:SNOW_USER, ($env:SNOW_PASS | ConvertTo-SecureString -AsPlainText -Force))
+            } else {
+                throw 'A ServiceNow server environment variable has been set, but authentication via SNOW_TOKEN or SNOW_USER/SNOW_PASS was not found'
+            }
         } else {
             throw "You must authenticate by either calling the New-ServiceNowSession cmdlet or passing in an Azure Automation connection object"
         }
