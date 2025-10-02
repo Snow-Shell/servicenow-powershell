92530f7cadea17bdd27d172bef19b04d8031909b
CATgwalker <78571293+CATgwalker@users.noreply.github.com>
2025-10-02 13:34:36 -0500

    Update Get-ServiceNowAuth.ps1
    Add Namespace parameter to support different API endpoints; defaults to 'now'

--- ServiceNow/Private/Get-ServiceNowAuth.ps1
+++ ServiceNow/Private/Get-ServiceNowAuth.ps1
@@ -13,10 +13,14 @@ function Get-ServiceNowAuth {
     [CmdletBinding()]
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'requirement of azure automation')]
 
-    Param (
+    param (
         [Parameter()]
         [Alias('C')]
         [hashtable] $Connection,
+
+        [Parameter()]
+        [Alias('N')]
+        [string] $Namespace = 'now',
 
         [Parameter()]
         [Alias('S')]
@@ -30,7 +34,11 @@ function Get-ServiceNowAuth {
     process {
 
         if ( $ServiceNowSession.Count -gt 0 ) {
-            $hashOut.Uri = $ServiceNowSession.BaseUri
+            if ($Namespace -ne 'now') {
+                $hashOut.Uri = $($ServiceNowSession.BaseUri -split ('api'))[0] + 'api/' + $Namespace
+            } else {
+                $hashOut.Uri = $ServiceNowSession.BaseUri
+            }
 
             # check if we need a new access token
             if ( $ServiceNowSession.ExpiresOn -lt (Get-Date) -and $ServiceNowSession.RefreshToken -and $ServiceNowSession.ClientCredential ) {
@@ -46,7 +54,7 @@ function Get-ServiceNowAuth {
                         refresh_token = $ServiceNowSession.RefreshToken.GetNetworkCredential().password
                     }
                 }
-            
+
                 $response = Invoke-RestMethod @refreshParams
 
                 $ServiceNowSession.AccessToken = New-Object System.Management.Automation.PSCredential('AccessToken', ($response.access_token | ConvertTo-SecureString -AsPlainText -Force))
@@ -64,8 +72,7 @@ function Get-ServiceNowAuth {
                 $hashOut.Headers = @{
                     'Authorization' = 'Bearer {0}' -f $ServiceNowSession.AccessToken.GetNetworkCredential().password
                 }
-            }
-            else {
+            } else {
                 # issue 248
                 $pair = '{0}:{1}' -f $ServiceNowSession.Credential.UserName, $ServiceNowSession.Credential.GetNetworkCredential().Password
                 $hashOut.Headers = @{ Authorization = 'Basic ' + [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair)) }
@@ -75,34 +82,28 @@ function Get-ServiceNowAuth {
                 $hashOut.Proxy = $ServiceNowSession.Proxy
                 if ( $ServiceNowSession.ProxyCredential ) {
                     $hashOut.ProxyCredential = $ServiceNowSession.ProxyCredential
-                }
-                else {
+                } else {
                     $hashOut.ProxyUseDefaultCredentials = $true
                 }
             }
-        }
-        elseif ( $Connection ) {
+        } elseif ( $Connection ) {
             Write-Verbose 'connection'
             # issue 248
             $pair = '{0}:{1}' -f $Connection.Username, $Connection.Password
             $hashOut.Headers = @{ Authorization = 'Basic ' + [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair)) }
-            $hashOut.Uri = 'https://{0}/api/now/v1' -f $Connection.ServiceNowUri
-        }
-        elseif ( $env:SNOW_SERVER ) {
-            $hashOut.Uri = 'https://{0}/api/now' -f $env:SNOW_SERVER
+            $hashOut.Uri = 'https://{0}/api/{1}/v1' -f $Connection.ServiceNowUri, $Namespace
+        } elseif ( $env:SNOW_SERVER ) {
+            $hashOut.Uri = 'https://{0}/api/{1}' -f $env:SNOW_SERVER, $Namespace
             if ( $env:SNOW_TOKEN ) {
                 $hashOut.Headers = @{
                     'Authorization' = 'Bearer {0}' -f $env:SNOW_TOKEN
                 }
-            }
-            elseif ( $env:SNOW_USER -and $env:SNOW_PASS ) {
+            } elseif ( $env:SNOW_USER -and $env:SNOW_PASS ) {
                 $hashOut.Credential = New-Object System.Management.Automation.PSCredential($env:SNOW_USER, ($env:SNOW_PASS | ConvertTo-SecureString -AsPlainText -Force))
-            }
-            else {
+            } else {
                 throw 'A ServiceNow server environment variable has been set, but authentication via SNOW_TOKEN or SNOW_USER/SNOW_PASS was not found'
             }
-        }
-        else {
+        } else {
             throw "You must authenticate by either calling the New-ServiceNowSession cmdlet or passing in an Azure Automation connection object"
         }
     }
